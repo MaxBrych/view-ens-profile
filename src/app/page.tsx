75188cc285f12281bce00df99ce2b319002e3970
MaxBrych <max.brych03@gmail.com>
2023-09-11 18:29:08 +0200

    activate pwa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import BundlrUpload from "@/components/BundlrUpload";
 import Hero from "@/components/Hero";
 import React from "react";
 
@@ -5,6 +6,7 @@ export default function Page() {
   return (
     <div className="flex flex-col items-start justify-start w-full min-h-screen p-4">
       <Hero />
+      <BundlrUpload />
     </div>
   );
 }
