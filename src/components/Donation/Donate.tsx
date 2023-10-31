31d0358fe3d0cb7da224e3d4d421594a685f2342
MaxBrych <max.brych03@gmail.com>
2023-10-31 20:46:20 +0100

    add Donate Drawer

--- src/components/Donation/Donate.tsx
+++ src/components/Donation/Donate.tsx
@@ -15,10 +15,12 @@ import {
   ModalOverlay,
   VStack,
   IconButton,
+  Heading,
 } from "@chakra-ui/react";
 import PublicDonation from "./PublicDonation";
 import PrivateDonation from "./PrivateDontation";
 import { BsCurrencyDollar } from "react-icons/bs";
+import { Drawer } from "vaul";
 
 interface DonateButtonProps {
   address: any;
@@ -30,80 +32,78 @@ export default function Donate({ address, name }: DonateButtonProps) {
 
   return (
     <>
-      <VStack>
-        <IconButton
-          colorScheme="white"
-          color="black"
-          bg="#05C756"
-          aria-label="Send Message"
-          icon={<BsCurrencyDollar />}
-          onClick={onOpen}
-          rounded={"full"}
-          size={"lg"}
-        />
-        {name && (
-          <Text
-            fontSize="xs"
-            className="font-semibold"
-            textColor={"blackAlpha.600"}
-          >
-            {name}
-          </Text>
-        )}
-      </VStack>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader fontStyle={"bold"}>Support</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody
-            mx={4}
-            mb={4}
-            className="border border-gray-300 rounded-xl"
-          >
-            <Tabs
-              variant="soft-rounded"
-              bg={"white"}
-              textColor={"black"}
-              colorScheme="whiteAlpha"
+      <Drawer.Root>
+        <Drawer.Trigger className="flex flex-col">
+          <IconButton
+            colorScheme="white"
+            color="black"
+            bg="#05C756"
+            aria-label="Send Message"
+            icon={<BsCurrencyDollar />}
+            onClick={onOpen}
+            rounded={"full"}
+            size={"lg"}
+          />
+          {name && (
+            <Text
+              fontSize="xs"
+              className="font-semibold"
+              textColor={"blackAlpha.600"}
             >
-              <TabList
-                w={"full"}
-                flex={1}
-                flexDirection={"row"}
-                justifyContent={"center"}
-                className="flex flex-row items-center justify-center flex-initial flex-shrink-0 p-1 my-4 bg-gray-100 rounded-full"
+              {name}
+            </Text>
+          )}
+        </Drawer.Trigger>
+
+        <Drawer.Portal>
+          <Drawer.Content>
+            <Heading fontStyle={"bold"}>Support</Heading>
+
+            <div className="mx-4 mb-4 border border-gray-300 rounded-xl">
+              <Tabs
+                variant="soft-rounded"
+                bg={"white"}
+                textColor={"black"}
+                colorScheme="whiteAlpha"
               >
-                <Tab
-                  fontSize={{ base: 12, md: 14 }}
-                  height={8}
-                  className="shadow-sm "
-                  textColor={"black"}
+                <TabList
+                  w={"full"}
+                  flex={1}
+                  flexDirection={"row"}
+                  justifyContent={"center"}
+                  className="flex flex-row items-center justify-center flex-initial flex-shrink-0 p-1 my-4 bg-gray-100 rounded-full"
                 >
-                  Public
-                </Tab>
-                <Tab
-                  fontSize={{ base: 12, md: 14 }}
-                  height={8}
-                  className="shadow-sm"
-                  textColor={"black"}
-                >
-                  Private
-                </Tab>
-              </TabList>
-              <TabPanels className="w-full">
-                <TabPanel className="w-full">
-                  <PublicDonation receiverAddress={address} />
-                </TabPanel>
-                <TabPanel>
-                  <PrivateDonation receiverAddress={address} />
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+                  <Tab
+                    fontSize={{ base: 12, md: 14 }}
+                    height={8}
+                    className="shadow-sm "
+                    textColor={"black"}
+                  >
+                    Public
+                  </Tab>
+                  <Tab
+                    fontSize={{ base: 12, md: 14 }}
+                    height={8}
+                    className="shadow-sm"
+                    textColor={"black"}
+                  >
+                    Private
+                  </Tab>
+                </TabList>
+                <TabPanels className="w-full">
+                  <TabPanel className="w-full">
+                    <PublicDonation receiverAddress={address} />
+                  </TabPanel>
+                  <TabPanel>
+                    <PrivateDonation receiverAddress={address} />
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+            </div>
+          </Drawer.Content>
+          <Drawer.Overlay />
+        </Drawer.Portal>
+      </Drawer.Root>
     </>
   );
 }
